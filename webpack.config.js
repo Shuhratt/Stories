28b9db04a1c3301dcc7bf5494d8bb86a5647783c
Shuhrat <shuh47@yandex.ru>
2021-02-15 20:35:00 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/js/index.js',
@@ -6,4 +8,42 @@ module.exports = {
     filename: 'js/main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      filename: 'leaders.html',
+      template: 'src/leaders.html',
+      minify: false,
+      scriptLoading: "blocking"
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css?[hash]',
+      chunkFilename: '[id].scss',
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    overlay: true,
+    open: true,
+    port: 9000
+  },
+  mode: 'production'
 };
