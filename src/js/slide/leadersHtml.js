465af6876fc12a38b57af85c9fea4f4debfa6615
Shuhrat <shuh47@yandex.ru>
2021-02-17 13:28:03 +0300

    1

--- src/js/slide/leadersHtml.js
+++ src/js/slide/leadersHtml.js
@@ -1,17 +1,7 @@
 export default (item) => {
   const html = `
-    <div class="slide__list-item slide__list-item_first">
-      <div class="slide__list-item-avatar">
-        <img class="slide__list-item-avatar-img" src="${item.avatar}" alt="" />
-        <div class="slide__list-item-emoji">👑</div>
-      </div>
-      <div class="slide__list-item-name">${item.name}</div>
-      <div class="slide__list-item-count">${item.valueText}</div>
-      <div class="slide__list-item-place slide__list-item-place_first">
-        <span class="slide__list-item-place-num">1</span>
-      </div>
-    </div>
+    <h1 class="slide__title">${item.title}</h1>
+    <h2 class="slide__subtitle">${item.subtitle}</h2>
   `
-
   return html
 }
--- src/js/stories.js
+++ src/js/stories.js
@@ -4,6 +4,18 @@ import leadersHtml from "./slide/leadersHtml";
 
 const app = document.getElementById('app');
 
+const url = new URL(window.location.href)
+const slideIndex = parseInt(url.searchParams.get('slide')) || 0
+const themeValue = url.searchParams.get('theme')
+
+const themes = {
+  light: 'theme_light',
+  dark: 'theme_dark'
+}
+
+themeValue ? document.body.classList.add(themes[themeValue]) : ''
+
+
 
 const data = async (url, method) => {
   let response = await fetch(url, {
@@ -20,7 +32,10 @@ const data = async (url, method) => {
 
 
 const renderTemplate =  async (alias, data) => {
-  console.log(await data)
+  const res = await data
+  const html = leadersHtml(res[slideIndex - 1].data)
+
+  app.insertAdjacentHTML('afterbegin', html)
   // return html со слайдом
 }
 renderTemplate('leaders', data('/data.json', 'GET'))
