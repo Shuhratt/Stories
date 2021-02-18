d6a36d076033a785f4fa67fcec6c7be45be9ec9b
Shuhrat <shuh47@yandex.ru>
2021-02-18 21:11:17 +0300

    Шаблоны

--- src/js/stories.js
+++ src/js/stories.js
@@ -1,53 +1,55 @@
-import '../scss/stories.scss'
+  import '../scss/stories.scss'
 
-import leadersHtml from "./slide/leadersHtml";
+  import leadersHtml from "./slide/leadersHtml";
 
+  const app = document.getElementById('app');
 
-const app = document.getElementById('app');
+  const url = new URL(window.location.href)
+  const slideIndex = parseInt(url.searchParams.get('slide')) >= 1  ? parseInt(url.searchParams.get('slide')) - 1 : 0
+  const themeValue = url.searchParams.get('theme') || null
+  console.log(slideIndex)
 
-const url = new URL(window.location.href)
-const slideIndex = parseInt(url.searchParams.get('slide')) >= 1  ? parseInt(url.searchParams.get('slide')) - 1 : 0
-const themeValue = url.searchParams.get('theme') || null
-console.log(slideIndex)
-
-const themes = {
-  light: 'theme_light',
-  dark: 'theme_dark'
-}
-themeValue ? document.body.classList.add(themes[themeValue]) : ''
-
-
-
-const data = async (url) => {
-  let response = await fetch(url);
-
-  if (response.ok) {
-    let json = await response.json();
-    return json
-  } else {
-    console.error("Ошибка HTTP: " + response.status);
+  const themes = {
+    light: 'theme_light',
+    dark: 'theme_dark'
   }
-}
-
-const template = {
-  leaders: (data, index) => {
-    return leadersHtml(data, index)
-  },
-  vote: () => {},
-  chart: () => {},
-  diagram: () => {},
-  activity: () => {}
-}
+  themeValue ? document.body.classList.add(themes[themeValue]) : ''
 
 
-const renderTemplate =  async (alias, data) => {
-  const res = await data
+  const template = {
+    leaders: (data, index) => {
+      return leadersHtml(data, index)
+    },
+    vote: () => {
+      console.error('Шаблона нет')
+    },
+    chart: () => {
+      console.error('Шаблона нет')
+    },
+    diagram: () => {
+      console.error('Шаблона нет')
+    },
+    activity: () => {
+      console.error('Шаблона нет')
+    }
+  }
+
+  const renderTemplate =  async (alias, data) => {
+    const html = template[alias](data, slideIndex)
+    app.insertAdjacentHTML('afterbegin', html)
+  }
+
+  const data = '/data.json'
+  fetch(data).then(response => response.json())
+    .then(result => {
+      const alias = result[slideIndex].alias
+      const data = result[slideIndex].data
+      renderTemplate(alias, data)
+
+    }).catch(e => {
+     console.error("Ошибка: " + e);
+    })
 
 
-  const html = template[alias](res[slideIndex].data, slideIndex)
 
-  app.insertAdjacentHTML('afterbegin', html)
-  // return html со слайдом
-}
-renderTemplate('leaders', data('/data.json'))
 
