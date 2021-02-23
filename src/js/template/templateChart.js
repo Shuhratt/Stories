9c9adca4936b9011fed1da7c3bccf3ac24103517
Shuhrat <shuh47@yandex.ru>
2021-02-23 16:54:53 +0300

    1

--- src/js/template/templateChart.js
+++ src/js/template/templateChart.js
@@ -1,9 +1,18 @@
 
 const templateChartHtml = (data) => {
+  const ratioHorizontal = 117/182,
+        ratioVertical = 182/270;
+
+  console.log('Horizontal:' + window.innerWidth > window.innerHeight)
+  console.log('Vertical:' + window.innerWidth < window.innerHeight)
+
+  const resultHeightCol = Math.floor(data.value * ratioHorizontal)
+  const adaptiveHeightCol = resultHeightCol / 376 * 100
+
   return `
     <div class="chart_col">
       <div class="chart_col-count">${data.value}</div>
-      <div class="chart_col-box" style="height: ${data.value}px"></div>
+      <div class="chart_col-box" style="height: ${adaptiveHeightCol}vh"></div>
       <div class="chart_col-num">${data.title}</div>
     </div>
   `
@@ -13,7 +22,7 @@ const templateChartUser = (data) => {
   return `
     <div class="chart__users-el">
       <div class="chart__users-el-avatar">
-        <img class="chart__users-el-avatar-img" src="images/2x/${data.avatar}"  alt="${data.name}" srcset="images/2x/${data.avatar} 1x, images/4x/${data.avatar} 2x">
+        <img class="chart__users-el-avatar-img" src="images/1x/${data.avatar}"  alt="${data.name}" srcset="images/1x/${data.avatar} 1x, images/2x/${data.avatar} 2x">
       </div>
       <div class="chart__users-el-info">
         <div class="chart__users-el-name">${data.name}</div>
@@ -23,14 +32,13 @@ const templateChartUser = (data) => {
   `
 }
 
-
-
 export default (item) => {
   const maxUsers = 2;
   const htmlHead = `
     <h1 class="app__title">${item.title}</h1>
     <h2 class="app__subtitle">${item.subtitle}</h2>
   `
+
   const chartBox = document.createElement('div')
   chartBox.className = 'chart__box';
 
@@ -38,6 +46,7 @@ export default (item) => {
   userList.className = 'chart__users'
 
   const chartHtml = item.values
+    .slice(-10, -1)
     .map((col) => templateChartHtml(col))
     .join('')
     .trim();
