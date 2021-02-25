6e4d7470659066964ede6bc5d3dc95de7d1d2962
Shuhrat <shuh47@yandex.ru>
2021-02-25 12:22:16 +0300

    diagramm

--- src/js/template/templateDiagram.js
+++ src/js/template/templateDiagram.js
@@ -1,31 +1,27 @@
-const templateListCommits = (data) => {
-
+const templateCircle = (data) => {
+  return `
+  <div class="chart_diagram__circle">
+      <svg class="chart_diagram__circle-chart" width="240" height="240" viewBox="0 0 285 285">
+        <circle class="chart_diagram__circle-chart-first" r="120" cx="50%" cy="50%"  stroke-dasharray="188.2 753"></circle>
+        <circle class="chart_diagram__circle-chart-second" r="120" cx="50%" cy="50%" stroke-dasharray="188.2 753" stroke-dashoffset="-188.2"></circle>
+        <circle class="chart_diagram__circle-chart-middle" r="120" cx="50%" cy="50%" stroke-dasharray="188.2 753" stroke-dashoffset="-376.5"></circle>
+        <circle class="chart_diagram__circle-chart-last" r="120" cx="50%" cy="50%" stroke-dasharray="188.2 753" stroke-dashoffset="-564.75"></circle>
+      </svg>
+     <div class="chart_diagram__circle-box">
+      <h3 class="chart_diagram__circle-title">${data.totalText}</h3>
+      <p class="chart_diagram__circle-result">${data.differenceText}</p>
+    </div>
+  </div>
+  `
 }
 
-
-const templateDiagramHtml = (data) => {
+const templateRowCommits = (data) => {
   return `
-    <div class="chart_diagram">
-      <div class="chart_diagram__circle">
-          <svg class="chart_diagram__circle-chart" width="240" height="240" viewBox="0 0 285 285">
-            <circle class="chart_diagram__circle-chart-first" r="120" cx="50%" cy="50%"  stroke-dasharray="188.2 753"></circle>
-            <circle class="chart_diagram__circle-chart-second" r="120" cx="50%" cy="50%" stroke-dasharray="188.2 753" stroke-dashoffset="-188.2"></circle>
-            <circle class="chart_diagram__circle-chart-middle" r="120" cx="50%" cy="50%" stroke-dasharray="188.2 753" stroke-dashoffset="-376.5"></circle>
-            <circle class="chart_diagram__circle-chart-last" r="120" cx="50%" cy="50%" stroke-dasharray="188.2 753" stroke-dashoffset="-564.75"></circle>
-          </svg>
-         <div class="chart_diagram__circle-box">
-          <h3 class="chart_diagram__circle-title">182 коммита</h3>
-          <p class="chart_diagram__circle-result">+42 с прошлого спринта</p>
-        </div>
-      </div>
-      <div class="chart_diagram__info">
-        <div class="chart_diagram__row">
-          <div class="chart_diagram__row-circle"></div>
-          <div class="chart_diagram__row-counts">> 1001 строки</div>
-          <div class="chart_diagram__row-progress">+8</div>
-          <div class="chart_diagram__row-result">30</div>
-        </div>
-      </div>
+    <div class="chart_diagram__row">
+      <div class="chart_diagram__row-circle"></div>
+      <div class="chart_diagram__row-counts">${data.title}</div>
+      <div class="chart_diagram__row-progress">${data.differenceText}</div>
+      <div class="chart_diagram__row-result">${data.valueText}</div>
     </div>
   `
 }
@@ -36,8 +32,16 @@ export default (item) => {
     <h1 class="app__title">${item.title}</h1>
     <h2 class="app__subtitle">${item.subtitle}</h2>
   `
+  const chart_diagram = document.createElement('div')
+  chart_diagram.className = 'chart_diagram';
+  chart_diagram.insertAdjacentHTML('afterbegin', templateCircle(item));
+  const chart_diagram__info = document.createElement('div')
+  chart_diagram__info.className = 'chart_diagram__info'
+  const htmlRow = item.categories.map(row => templateRowCommits(row)).join('')
 
+  chart_diagram__info.insertAdjacentHTML('afterbegin', htmlRow )
+  chart_diagram.insertAdjacentHTML('beforeend', chart_diagram__info.outerHTML )
 
-  return htmlHead.concat(templateDiagramHtml(item))
+  return htmlHead.concat(chart_diagram.outerHTML)
 
 }
