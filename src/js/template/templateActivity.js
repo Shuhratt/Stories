3cef369e6284f2786ff14c865f5a87c7db87fcd9
Shuhrat <shuh47@yandex.ru>
2021-02-26 12:32:11 +0300

    canvas

--- src/js/template/templateActivity.js
+++ src/js/template/templateActivity.js
@@ -2,13 +2,22 @@ import templateHead from "./templateHead";
 
 const draw = () => {
   const canvas = document.createElement('canvas');
-  canvas.width = 442;
-  canvas.height = 112;
+  canvas.width = 900;
+  canvas.height = 200;
   const ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  ctx.beginPath();
+  ctx.moveTo(0,0);
+  ctx.lineTo(50, 50);
+  ctx.lineTo(50, 150);
+  ctx.lineTo(0, 100);
   ctx.fillStyle = 'blue';
-  ctx.fillRect(10, 10, 100, 100);
+
+
+  ctx.stroke();
+  ctx.fill();
+
 
   document.body.appendChild(canvas)
 }
