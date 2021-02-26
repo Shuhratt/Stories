ba769c16140f7a81f7b40c73e699da1abc6beeba
Shuhrat <shuh47@yandex.ru>
2021-02-26 15:36:32 +0300

    canvas

--- src/js/template/templateActivity.js
+++ src/js/template/templateActivity.js
@@ -2,24 +2,79 @@ import templateHead from "./templateHead";
 
 const draw = () => {
   const canvas = document.createElement('canvas');
-  canvas.width = 900;
-  canvas.height = 200;
+  const width = 425/668 * 100;
+  const height = 112/376 * 100
+  canvas.style.width = `${width}vw`;
+  canvas.style.height =`${height}vh`;
+  canvas.style.marginLeft = 'auto'
+  canvas.style.marginRight = 'auto'
+  canvas.style.display = 'block'
+
   const ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.beginPath();
-  ctx.moveTo(0,0);
-  ctx.lineTo(50, 50);
-  ctx.lineTo(50, 150);
-  ctx.lineTo(0, 100);
-  ctx.fillStyle = 'blue';
+  const drawCube = (x, y, wx, wy, h, color) => {
+
+    //left
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x - wx, y - wy); // 2
+    ctx.lineTo(x - wx, y + h); // 3
+    ctx.lineTo(x, y + wy + h); // 4
+    ctx.closePath();
+    ctx.fillStyle = color;
+    ctx.stroke();
+    ctx.fill();
+
+    //right
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + wx, y - wy); // 2
+    ctx.lineTo(x + wx, y + h); // 3
+    ctx.lineTo(x, y + wy + h); // 4
+    ctx.closePath();
+    ctx.fillStyle = color;
+    ctx.stroke();
+    ctx.fill();
+
+    //top
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x - wx, y - wy); // 2
+    ctx.lineTo(x, y - wy - h); // 3
+    ctx.lineTo(x + wx, y - wy); // 4
+
+    ctx.closePath();
+    ctx.fillStyle = color;
+    ctx.stroke();
+    ctx.fill();
+
+  }
+  // a = 30.26 / строна ромба
+
+  drawCube(
+    30, // loop
+    60, // loop
+    30,
+    30,
+    30,
+    'blue'
+  ); // fig 1
+  drawCube(
+    90,
+    60,
+    30,
+    30,
+    30,
+    'red'
+  );// fig 2
 
 
-  ctx.stroke();
-  ctx.fill();
 
 
-  document.body.appendChild(canvas)
+
+
+  document.body.append(canvas)
 }
 
 
