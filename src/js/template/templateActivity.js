329a1f4080724413066a090d823e90290794362e
Shuhrat <shuh47@yandex.ru>
2021-03-01 14:49:34 +0300

    canvas

--- src/js/template/templateActivity.js
+++ src/js/template/templateActivity.js
@@ -14,8 +14,7 @@ const draw = () => {
 
   const ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.translate(0, canvas.height);
-  ctx.rotate(-Math.PI/2); // поворот
+
 
   const drawCube = (x, y, wx, wy, h = 8, gradient = []) => {
 
@@ -23,10 +22,10 @@ const draw = () => {
     ctx.beginPath();
     ctx.moveTo(x, y);
     ctx.lineTo(x - wx, y - wy); // 2
-    ctx.lineTo(x - wx, y + h); // 3
-    ctx.lineTo(x, y + wy + h); // 4
-    ctx.closePath();
+    ctx.lineTo(x - wx, y - wy - h); // 3
+    ctx.lineTo(x, y - h); // 4
 
+    ctx.closePath();
     const color = '#fff'
     const gradientLeft = ctx.createLinearGradient(x - wx, y - wy, x - wx, y + h);// 2,3
     gradientLeft.addColorStop(0, 'rgba(62, 57, 52, 0.7)');
@@ -36,55 +35,57 @@ const draw = () => {
     ctx.stroke();
     ctx.fill();
 
-    // //right
-    // ctx.beginPath();
-    // ctx.moveTo(x, y);
-    // ctx.lineTo(x + wx, y - wy); // 2
-    // ctx.lineTo(x + wx, y + h); // 3
-    // ctx.lineTo(x, y + wy + h); // 4
-    // ctx.closePath();
-    //
-    // const gradientRight= ctx.createLinearGradient(x + wx, y - wy, x + wx, y + h);// 2,3
-    // gradientRight.addColorStop(0, 'rgba(62, 57, 52, 0.7)');
-    // gradientRight.addColorStop(1, 'rgba(34, 31, 28, 0.7)');
-    //
-    // ctx.fillStyle = color;
-    // ctx.stroke();
-    // ctx.fill();
-    //
+
+    //right
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x, y - h); // 2
+    ctx.lineTo(x + wx, y - h - wy); // 3
+    ctx.lineTo(x + wx, y - wy); // 4
+    ctx.closePath();
+
+    const gradientRight= ctx.createLinearGradient(x + wx, y - wy, x + wx, y + h);// 2,3
+    gradientRight.addColorStop(0, 'rgba(62, 57, 52, 0.7)');
+    gradientRight.addColorStop(1, 'rgba(34, 31, 28, 0.7)');
+
+    ctx.fillStyle = color;
+    ctx.stroke();
+    ctx.fill();
+
     // //top
-    // ctx.beginPath();
-    // ctx.moveTo(x, y);
-    // ctx.lineTo(x - wx, y - wy); // 2
-    // ctx.lineTo(x, y - wy * 2); // 3
-    // ctx.lineTo(x + wx, y - wy); // 4
-    //
-    // const gradientTop = ctx.createLinearGradient(x - wx, y - wy, x, y - wy * 2);// 2,3
-    // gradientTop.addColorStop(0, 'rgba(62, 57, 52, 0.7)');
-    // gradientTop.addColorStop(1, 'rgba(34, 31, 28, 0.7)');
-    //
-    // ctx.closePath();
-    // ctx.fillStyle = color;
-    // ctx.stroke();
-    // ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(x, y - h);
+    ctx.lineTo(x - wx, y - wy - h); // 2
+    ctx.lineTo(x, y - h - wy * 2); // 3
+    ctx.lineTo(x + wx, y - h - wy); // 4
+
+    const gradientTop = ctx.createLinearGradient(x - wx, y - wy, x, y - wy * 2);// 2,3
+    gradientTop.addColorStop(0, 'rgba(62, 57, 52, 0.7)');
+    gradientTop.addColorStop(1, 'rgba(34, 31, 28, 0.7)');
+
+    ctx.closePath();
+    ctx.fillStyle = color;
+    ctx.stroke();
+    ctx.fill();
+
 
   }
   // Сторона ромба равна 20.808652046684813
 
   drawCube(
     20, // первая точка
-    25, // первый ряд
+    50, // первый ряд
     17,
     12,
-    8,
+    20,
   ); // fig 1
-  // drawCube(
-  //   20,// шаг 34
-  //   40,
-  //   17,
-  //   12,
-  //   8,
-  // );// fig 2
+  drawCube(
+    54,// шаг 34
+    50,
+    17,
+    12,
+    10,
+  );// fig 2
   // drawCube(
   //   88,
   //   25,
