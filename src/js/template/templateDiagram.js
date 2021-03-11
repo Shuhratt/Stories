b93209463fff8f29528d8b2d6c471bfe901ada5b
Shuhrat <shuh47@yandex.ru>
2021-03-11 12:15:10 +0300

    diagram

--- src/js/template/templateDiagram.js
+++ src/js/template/templateDiagram.js
@@ -4,10 +4,10 @@ const templateCircle = (data) => {
   return `
     <div class="chart_diagram__circle">
       <svg class="chart_diagram__circle-chart" width="240" height="240" viewBox="0 0 285 285">
-        <circle class="chart_diagram__circle-chart-first" r="120" cx="50%" cy="50%"  stroke-dasharray="188.2 753"></circle>
-        <circle class="chart_diagram__circle-chart-second" r="120" cx="50%" cy="50%" stroke-dasharray="188.2 753" stroke-dashoffset="-188.2"></circle>
-        <circle class="chart_diagram__circle-chart-middle" r="120" cx="50%" cy="50%" stroke-dasharray="188.2 753" stroke-dashoffset="-376.5"></circle>
-        <circle class="chart_diagram__circle-chart-last" r="120" cx="50%" cy="50%" stroke-dasharray="188.2 753" stroke-dashoffset="-564.75"></circle>
+        <circle class="chart_diagram__circle-chart-first" r="120" cx="50%" cy="50%"  stroke-dasharray="188.4 753.6"></circle>
+        <circle class="chart_diagram__circle-chart-second" r="120" cx="50%" cy="50%" stroke-dasharray="188.4 753.6" stroke-dashoffset="-188.4"></circle>
+        <circle class="chart_diagram__circle-chart-middle" r="120" cx="50%" cy="50%" stroke-dasharray="188.4 753.6" stroke-dashoffset="-376.8"></circle>
+        <circle class="chart_diagram__circle-chart-last" r="120" cx="50%" cy="50%" stroke-dasharray="188.4 753.6" stroke-dashoffset="-565.2"></circle>
       </svg>
       <div class="chart_diagram__circle-box">
        <h3 class="chart_diagram__circle-title">${data.totalText}</h3>
