28cc0d77cfd0d589db69a19a705a84c37c591c1c
Shuhrat <shuh47@yandex.ru>
2021-03-15 13:53:20 +0300

    leaders

--- src/js/template/templateLeaders.js
+++ src/js/template/templateLeaders.js
@@ -1,15 +1,20 @@
 import templateHead from "./templateHead";
 
 const templateHtml = (data, emoji, like, index) => {
+  const [firstName, lastName] = data.name.split(' ')
   return `
     <div class="leaders__list-item">
-      <div class="leaders__list-item-avatar">
-        <img class="leaders__list-item-avatar-img" src="images/2x/${data.avatar}"  alt="${data.name}" srcset="images/2x/${data.avatar} 1x,
-        images/4x/${data.avatar} 2x" />
+      <div class="leaders__list-item-box">
         <div class="leaders__list-item-emoji">${index + 1 === 1 ? emoji : like }</div>
+        <div class="leaders__list-item-avatar">
+          <img class="leaders__list-item-avatar-img" src="images/2x/${data.avatar}"  alt="${data.name}" srcset="images/2x/${data.avatar} 1x, images/4x/${data.avatar} 2x" />
+        </div>
+        <div class="leaders__list-item-name">
+          <span>${firstName}</span>  
+          <span>${lastName}</span>
+        </div>
+        <div class="leaders__list-item-count">${data.valueText}</div>
       </div>
-      <div class="leaders__list-item-name">${data.name}</div>
-      <div class="leaders__list-item-count">${data.valueText}</div>
       <div class="leaders__list-item-place">
         <span class="leaders__list-item-place-num">${index + 1}</span>
       </div>
