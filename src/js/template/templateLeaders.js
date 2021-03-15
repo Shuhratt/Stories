54893337b3c8b8ee748f7f057aebf63451687227
Shuhrat <shuh47@yandex.ru>
2021-03-15 16:36:55 +0300

    leader

--- src/js/template/templateLeaders.js
+++ src/js/template/templateLeaders.js
@@ -3,10 +3,13 @@ import templateHead from "./templateHead";
 const templateHtml = (data, emoji, like, index, selector) => {
   const [firstName, lastName] = data.name.split(' ');
   const dataLike = `data-like=${like}`
+  const voteItemClass = `leaders__list-item${selector}`
+  const voteBox = `leaders__list-item-box${selector}`
+  const votePlace = `leaders__list-item-place${selector}`
 
   return `
-    <div class="leaders__list-item ${index + 1 === 5 ? selector : ''}" ${index + 1 === 5 ? dataLike : ''} >
-      <div class="leaders__list-item-box">
+    <div class="leaders__list-item ${index + 1 === 5 ? voteItemClass : ''}" ${index + 1 === 5 ? dataLike : ''} >
+      <div class="leaders__list-item-box ${index + 1 === 5 ? voteBox : ''}">
         <div class="leaders__list-item-emoji">${index + 1 === 1 ? emoji : '' }</div>
         <div class="leaders__list-item-avatar">
           <img class="leaders__list-item-avatar-img" src="images/2x/${data.avatar}"  alt="${data.name}" srcset="images/2x/${data.avatar} 1x, images/4x/${data.avatar} 2x" />
@@ -17,7 +20,7 @@ const templateHtml = (data, emoji, like, index, selector) => {
         </div>
         <div class="leaders__list-item-count">${data.valueText}</div>
       </div>
-      <div class="leaders__list-item-place">
+      <div class="leaders__list-item-place ${index + 1 === 5 ? votePlace : ''}">
         <span class="leaders__list-item-place-num">${index + 1}</span>
       </div>
     </div>
@@ -34,7 +37,7 @@ export default (item) => {
   const slide__list = document.createElement('div')
   slide__list.className = 'leaders__list'
 
-  const selectorVotePortrait = item.hasOwnProperty('selectedUserId') ? 'leaders__list-item_vote' : ''
+  const selectorVotePortrait = item.hasOwnProperty('selectedUserId') ? '_vote' : ''
   const usersHtml = item.users
     .slice(0, maxUsers)
     .map((user, index) => templateHtml(user, item.emoji, '👍', index, selectorVotePortrait))
