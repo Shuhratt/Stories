7abbd40419aaed7c96681acbc8dbab2478b7643a
Shuhrat <shuh47@yandex.ru>
2021-03-18 11:21:51 +0300

    diagram

--- src/js/template/templateDiagram.js
+++ src/js/template/templateDiagram.js
@@ -7,10 +7,10 @@ const templateCircle = (data) => {
   return `
     <div class="chart_diagram__circle">
       <svg class="chart_diagram__circle-chart" width="240" height="240" viewBox="0 0 285 285">
-        <circle class="chart_diagram__circle-chart-first" r="120" cx="50%" cy="50%"  stroke-dasharray="122.3 753.6" stroke-linecap="round" stroke-mitterlimit="0" transform="rotate(-90 ) translate(-285 0)"></circle>
-<!--        <circle class="chart_diagram__circle-chart-second" r="120" cx="50%" cy="50%" stroke-dasharray="131.6 753.6" stroke-dashoffset="251.28"></circle>-->
-<!--        <circle class="chart_diagram__circle-chart-middle" r="120" cx="50%" cy="50%" stroke-dasharray="188.4 753.6" stroke-dashoffset="-376.8"></circle>-->
-<!--        <circle class="chart_diagram__circle-chart-last" r="120" cx="50%" cy="50%" stroke-dasharray="188.4 753.6" stroke-dashoffset="-565.2"></circle>-->
+        <circle class="chart_diagram__circle-chart-first" r="120" cx="50%" cy="50%"  stroke-dasharray="122.3 753.6" transform="rotate(-120.38 )" transform-origin="center" ></circle>
+        <circle class="chart_diagram__circle-chart-second" r="120" cx="50%" cy="50%" stroke-dasharray="131.6 753.6" transform="rotate(-60.86 )" transform-origin="center"></circle>
+        <circle class="chart_diagram__circle-chart-middle" r="120" cx="50%" cy="50%" stroke-dasharray="236.85 753.6" transform="rotate(3.09 )" transform-origin="center"></circle>
+        <circle class="chart_diagram__circle-chart-last" r="120" cx="50%" cy="50%" stroke-dasharray="253.43 753.6" transform="rotate(117.2 )" transform-origin="center"></circle>
       </svg>
       <div class="chart_diagram__circle-box">
        <h3 class="chart_diagram__circle-title">${totalText}</h3>
