d8719f84fe9e4541abb3d3a9e8688ac1c68d321f
Shuhrat <shuh47@yandex.ru>
2021-03-21 10:56:27 +0300

    scsslinter

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
-
+  plugins: ['stylelint-csstree-validator'],
+  extends: [
+    'stylelint-config-recommended-scss',
+    'stylelint-config-rational-order'
+  ],
+  rules: {
+    'csstree/validator': true,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global']
+      }
+    ],
+    'selector-class-pattern': '^[a-z-_]+$'
+  }
 }
